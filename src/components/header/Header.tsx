32696de2257af9ab076b7273be3452edc98f6887
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-10-08 00:34:41 +0900

    add basename

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,16 +6,12 @@ import {
 import { faHouse, faCode } from "@fortawesome/free-solid-svg-icons";
 import kyoropon from "/kyoropon.svg";
 import NavItem from "./NavItem";
-import BASE_URL from "@/router/basename";
 
 const Header = () => {
   return (
     <header className="w-full h-20 p-4 z-50 sticky top-0 bg-slate-700 flex items-center justify-between opacity-80">
       <NavigationMenu>
-        <NavigationMenuLink
-          href={BASE_URL + "/secret"}
-          className="hover:opacity-70"
-        >
+        <NavigationMenuLink href="/secret" className="hover:opacity-70">
           <img src={kyoropon} className="h-20" />
         </NavigationMenuLink>
       </NavigationMenu>
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,12 +3,12 @@ import Home from "@/pages/Home";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Secret from "@/pages/Secret";
 import Works from "@/pages/Works";
-// import BASE_URL from "./basename";
+import BASE_URL from "./basename";
 
 const AppRouter = () => {
   return (
     <>
-      <HashRouter>
+      <HashRouter basename={BASE_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/works" element={<Works />} />
